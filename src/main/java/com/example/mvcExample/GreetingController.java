aa1767c6baca4a4aaea4dffe00f3218a6ce43e3c
Bananahanack <jurichus94@gmail.com>
2020-01-24 23:45:52 +0300

    Spring frame

--- src/main/java/com/example/mvcExample/GreetingController.java
+++ src/main/java/com/example/mvcExample/GreetingController.java
@@ -2,7 +2,6 @@ package com.example.mvcExample;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,9 +11,15 @@ import java.util.Map;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        model.put("some", "lol");
+        return "main";
     }
 
 }
